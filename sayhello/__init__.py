f536f6606a6525bd8690ffe89948702cf291d124
shihao.zhou <32856856+OoGod@users.noreply.github.com>
2024-12-06 13:49:55 +0000

    fix proxy problem

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -9,11 +9,14 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 app = Flask('sayhello')
 app.config.from_pyfile('settings.py')
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
